f523e9b8306a43a1c61e35ebb176f168a2b99dc8
dealer426 <burnssamuel01@gmail.com>
2020-01-25 17:10:36 -0500

    Added Numeric Testing with Methods

---
+++ TDDexamples.Tests/NumericValuesTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TDDexamples.Tests
+{
+    public class NumericValuesTests
+    {
+
+        [Fact]
+        public void Add_GivenTwoIntValues_Returns_Int()
+        {
+            //Arrange
+            var myNumbers = new NumericValues();
+
+            //Act
+            var result = myNumbers.Add(1, 2);
+
+            //Assert
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Add_GivenTwoDoubleValues_Returns_Double()
+        {
+            //Arrange
+            var myNumbers = new NumericValues();
+
+            //Act
+            var result = myNumbers.AddDouble(1.2, 4.5);
+
+            //Assert
+            Assert.Equal(5.7, result);
+        }
+
+        [Fact]
+        public void Multiply_GivenTwoIntValues_Returns_Int()
+        {
+            //Arrange
+            var myNumbers = new NumericValues();
+
+            //Act
+            var result = myNumbers.Multiply(2, 3);
+
+            //Assert
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Subtract_GivenTwoIntValues_Returns_Int()
+        {
+            //Arrange
+            var myNumbers = new NumericValues();
+
+            //Act
+            var result = myNumbers.Subtract(6, 4);
+
+            //Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Decrement_Age_Property_Returns_One_Less_From_original_Value()
+        {
+            //Arrange
+            var myNumbers = new NumericValues();
+
+            //Act
+            var startingAge = myNumbers.Age;
+            var decramentAge = myNumbers.DecrementAge();
+
+            //Assert
+            Assert.Equal(startingAge -1, decramentAge);
+        }
+
+        [Fact]
+        public void Increment_Age_Property_Returns_One_More_From_original_Value()
+        {
+            //Arrange
+            var myNumbers = new NumericValues();
+
+            //Act
+            var startingAge = myNumbers.Age;
+            var incrementAge = myNumbers.IncrementAge();
+
+            //Assert
+            Assert.Equal(startingAge + 1, incrementAge);
+        }
+
+
+        [Fact]
+        public void Add5_Age_Property_Returns_5_More_From_original_Value()
+        {
+            //Arrange
+            var myNumbers = new NumericValues();
+
+            //Act
+            var startingAge = myNumbers.Age;
+            var incrementAge = myNumbers.Add5ToAge();
+
+            //Assert
+            Assert.Equal(startingAge + 5, incrementAge);
+        }
+
+
+
+    }
+}
---
+++ TDDexamples/NumericValues.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDexamples
+{
+    public class NumericValues
+    {
+
+        public int Age { get; set; } = 5;
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+
+        public double AddDouble(double a, double b)
+        {
+            return a + b;
+        }
+
+        public int DecrementAge()
+        {
+            return --Age;
+        }
+
+        public int IncrementAge()
+        {
+            return ++Age;
+        }
+
+        public int Add5ToAge()
+        {
+            return Age += 5;
+        }
+
+
+
+
+    }
+}

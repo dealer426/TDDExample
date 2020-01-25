e53d7e5959e5bc632f63dce73c4f9371ccd795fe
dealer426 <burnssamuel01@gmail.com>
2020-01-25 16:35:09 -0500

    added Grocery Class for testing lists

---
+++ TDDexamples.Tests/GroceryClassTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace TDDexamples.Tests
+{
+    public class GroceryClassTests
+    {
+        [Fact]
+        public void ShouldCreateGroceryObject()
+        {
+            var grocery = new GroceryClass("Sav-A-Lot");
+
+        }
+
+        [Fact]
+        public void ShouldGetOurList_Do_We_Have_a_list()
+        {
+            // Arrange
+            var sut = new GroceryClass();
+
+            // Act
+            var myList = sut.ReturnMyList();
+
+            // Assert
+            Assert.IsType<List<string>>(myList);
+        }
+
+        [Fact]
+        public void Can_We_Add_To_The_List()
+        {
+            var sut = new GroceryClass();
+
+            int initialListCount = sut.ReturnMyList().Count;
+            sut.AddStuffToList("stuff");
+            var myList = sut.ReturnMyList();
+
+            Assert.Equal(initialListCount + 1, myList.Count);
+        }
+    }
+
+}
---
+++ TDDexamples/GroceryClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDexamples
+{
+    public class GroceryClass
+    {
+        public List<string> StuffToBuy = new List<string>();
+
+        public GroceryClass()
+        {
+
+        }
+
+        public GroceryClass(string name)
+        {
+
+        }
+
+        public void AddStuffToList(string stuff)
+        {
+            StuffToBuy.Add(stuff);
+        }
+
+        public List<string> ReturnMyList()
+        {
+            return StuffToBuy;
+        }
+
+
+    }
+}

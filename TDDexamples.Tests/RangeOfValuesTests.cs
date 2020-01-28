cd1a3aeae00b255cc8c9c12f6498d50dd83c4df6
dealer426 <burnssamuel01@gmail.com>
2020-01-28 07:54:08 -0500

    Added Testing a Range of Numbers

---
+++ TDDexamples.Tests/RangeOfValuesTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TDDexamples.Tests
+{
+    public class RangeOfValuesTests
+    {
+        [Fact]
+        public void Name_Name_Is_Not_Empty()
+        {
+            //Arange
+            var person = new RangeOfValues();
+
+            //Act
+
+            //Assert
+
+            //Check to make sure its not Null
+            Assert.NotNull(person.Name);
+
+            //Check to make sure the string i snot empty
+            Assert.False(string.IsNullOrEmpty(person.Name));
+        }
+
+        [Fact]
+        public void Age_Does_It_Fall_Within_A_Range()
+        {
+            //Arrange
+            var person = new RangeOfValues();
+
+            //Act
+
+            //Assert
+            Assert.InRange(person.Age, 25, 60);
+        }
+
+    }
+}
---
+++ TDDexamples/RangeOfValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDexamples
+{
+    public class RangeOfValues
+    {
+        public string Name => "Sam";
+        public int Age => 52;
+
+    }
+}
